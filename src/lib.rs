b56da026e14232fd75fcf68ca7de0f0070400c79
bltzxsd <blissardartinq@gmail.com>
2022-01-13 16:59:44 +0530

    update docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,32 @@
 //! This library provides easy access to the Discord IPC.
-//! 
+//!
 //! It provides implementations for both Unix and Windows
 //! operating systems, with both implementations using the
 //! same API. Thus, this crate can be used in a platform-agnostic
 //! manner.
-//! 
+//!
 //! # Hello world
-//! ```
+//! ```text
 //! use discord_rich_presence::{activity, new_client, DiscordIpc};
-//! 
-//! fn main() -> Result<(), Box<dyn std::error::Error>> {
+//! use std::error::Error;
+//! fn main() -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
 //!     let mut client = new_client("<some client id>")?;
 //!     client.connect()?;
-//! 
+//!
 //!     let payload = activity::Activity::new().state("Hello world!");
 //!     client.set_activity(payload)?;
-//! }
+//!
+//!     Ok(())
+//!  }
 //! ```
 #![deny(missing_docs)]
 
 mod discord_ipc;
+
 mod pack_unpack;
 pub use discord_ipc::*;
 pub mod activity;
+mod error;
 
 #[cfg(unix)]
 mod ipc_unix;
@@ -34,14 +38,15 @@ mod ipc_windows;
 #[cfg(windows)]
 use ipc_windows as ipc;
 
-
 /// Creates a new client to connect to the Discord IPC.
-/// 
+///
 /// # Examples
 /// ```
-/// let ipc_client = discord_ipc_client::new_client("<some client id>")?;
+/// # use discord_rich_presence::new_client;
+/// let ipc_client = new_client("<some client id>")?;
+/// # Ok::<(), Box<dyn std::error::Error + Send + Sync + 'static>>(())
 /// ```
-pub fn new_client(client_id: &str) -> Result<impl DiscordIpc, Box<dyn std::error::Error>> {
+pub fn new_client(client_id: &str) -> Result<impl DiscordIpc, crate::error::Error> {
     let client = ipc::DiscordIpcClient {
         client_id: client_id.to_string(),
         connected: false,
